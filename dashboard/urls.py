c77ee38f5507d1090d9aa0fb24c21a7cfaf93601
ThomasGumede <gumedethomas@outlook.com>
2025-11-09 09:36:36 +0200

    file improves

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dashboard.views.home import index, clan_meetings, clan_documents, get_clan_meetings_api
+from dashboard.views.home import download_file, index, clan_meetings, clan_documents, get_clan_meetings_api
 
 app_name = 'dashboard'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('meetings', clan_meetings, name='clan-meetings'),
     path('documents', clan_documents, name='clan-documents'),
     path('api/meetings', get_clan_meetings_api, name='get-meetings-api'),
+    path('documents/<file_id>', download_file, name='download-file'),
 ]
--- dashboard/views/home.py
+++ dashboard/views/home.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from dashboard.models import ClanDocument, Meeting
 from django.core import serializers
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+import logging, mimetypes
+
+logger = logging.getLogger("events")
 
 @login_required
 def index(request):
@@ -26,3 +30,25 @@ def get_clan_meetings_api(request):
         return JsonResponse({"success": True, "meetings": data}, status=200)
     except Exception as ex:
         return JsonResponse({"success": False, "message": f"Something went wrong: {ex}"}, status=200)
+    
+@login_required    
+def download_file(request, file_id):
+    media = get_object_or_404(ClanDocument.objects.all(), id=file_id)
+    
+    try:
+            file_path = media.file.path
+            file_name = media.file.name
+            if file_path and file_name:
+                with open(file_path, 'rb') as file:
+                    file_data = file.read()
+                    mime_type, _ = mimetypes.guess_type(file_path)
+                    mime_type = mime_type or 'application/octet-stream'
+                    response = HttpResponse(file_data, content_type=mime_type)
+                    
+                response['Content-Disposition'] = f'attachment; filename="{file_name.split("/")[-1]}"'
+        
+            return response
+    except Exception as ex:
+        logger.error("Missing Media file")
+        messages.error(request, "Media file not aploaded yet, send us an email if you have questions")
+        return redirect("dashboard:clan-documents")
